12966f9f888486ea2a2d8a2cf31eee3798e9c38e
SamuraiFree <42548698+SamuraiFree@users.noreply.github.com>
2019-07-16 02:55:38 +0300

    Add files via upload

---
+++ BotAnistar_V_1.10.py
@@ -0,0 +1,31 @@
+import telebot
+import random
+
+token = 'TOKEN_TG'
+
+mas = ["AwADAgADcQQAAoREaEna38mVwAQFUAI","AwADAgADSQUAAl-uYEnfeal9RBCGTwI","AwADAgADUgMAAoDgaEmL_YJE9ZIbJgI","AwADAgADUwMAAoDgaEkNTnUo3FC-kAI","AwADAgADKAUAAiR2aEmJLCMrfNQK8gI"]
+# AwADAgADLAQAAicSaUlvogAB8Y6wZjoC ой да  ладно тебе, мы же хорошие, ня!
+
+
+bot = telebot.TeleBot(token)
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+    
+#Болталка
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text == 'Привет':
+        bot.send_message(message.chat.id, 'Приветствую тебя мой повелитель');
+        
+    elif message.text =='Пока' :
+        bot.send_message(message.chat.id, 'Я буду ждать вашего возвращения господин')
+        
+    
+        
+#приветствие новых пользователей
+@bot.message_handler(content_types=["new_chat_members"])
+def handler_new_member(message):
+    bot.send_voice(message.chat.id, voice=(mas[random.randint(0,4)]) )
+    
+bot.polling()
---
+++ Procfile
@@ -0,0 +1,1 @@
+worked: python BotAnistar_V_1.10.py
