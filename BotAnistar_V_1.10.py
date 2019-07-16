221f3b390ea82b223b143048668fee377768b76b
SamuraiFree <42548698+SamuraiFree@users.noreply.github.com>
2019-07-16 14:49:17 +0300

    Update BotAnistar_V_1.10.py

--- BotAnistar_V_1.10.py
+++ BotAnistar_V_1.10.py
@@ -1,11 +1,12 @@
 import telebot
-import random
+#import random
 
-token = 'TOKEN_TG'
+token = '861985547:AAEN61AyePKOwwwcnYOPI6VLvwZDF6YpqEI'
 
-mas = ["AwADAgADcQQAAoREaEna38mVwAQFUAI","AwADAgADSQUAAl-uYEnfeal9RBCGTwI","AwADAgADUgMAAoDgaEmL_YJE9ZIbJgI","AwADAgADUwMAAoDgaEkNTnUo3FC-kAI","AwADAgADKAUAAiR2aEmJLCMrfNQK8gI"]
+#helloNew = ["AwADAgADcQQAAoREaEna38mVwAQFUAI","AwADAgADSQUAAl-uYEnfeal9RBCGTwI","AwADAgADUgMAAoDgaEmL_YJE9ZIbJgI","AwADAgADUwMAAoDgaEkNTnUo3FC-kAI","AwADAgADKAUAAiR2aEmJLCMrfNQK8gI","AwADAgADRgMAAgfAaEmWyuNUpVtvEQI"]
 # AwADAgADLAQAAicSaUlvogAB8Y6wZjoC ой да  ладно тебе, мы же хорошие, ня!
-
+#Hello = ["AwADAgADRwMAAgfAaEliw-kNF6wJMgI"]
+#Bi = ["AwADAgAD4QIAAkXiaUldUVt0cRJyvwI"]
 
 bot = telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
@@ -13,19 +14,19 @@ def start_message(message):
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
     
 #Болталка
-@bot.message_handler(content_types=['text'])
-def send_text(message):
-    if message.text == 'Привет':
-        bot.send_message(message.chat.id, 'Приветствую тебя мой повелитель');
-        
-    elif message.text =='Пока' :
-        bot.send_message(message.chat.id, 'Я буду ждать вашего возвращения господин')
+#@bot.message_handler(content_types=['text'])
+#def send_text(message):
+#    if message.text == 'Привет':
+#        bot.send_voice(message.chat.id, voice=(Hello[0]) )
+#        
+#    elif message.text =='Пока' :
+#        bot.send_voice(message.chat.id, voice=(Bi[0]) )
         
     
         
 #приветствие новых пользователей
-@bot.message_handler(content_types=["new_chat_members"])
-def handler_new_member(message):
-    bot.send_voice(message.chat.id, voice=(mas[random.randint(0,4)]) )
+#@bot.message_handler(content_types=["new_chat_members"])
+#def handler_new_member(message):
+#    bot.send_voice(message.chat.id, voice=(helloNew[random.randint(0,5)]) )
     
 bot.polling()
