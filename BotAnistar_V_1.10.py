44876ea915b12297ffc879512fc4f5689228be3c
SamuraiFree <42548698+SamuraiFree@users.noreply.github.com>
2019-07-29 19:41:24 +0300

    Update BotAnistar_V_1.10.py

--- BotAnistar_V_1.10.py
+++ BotAnistar_V_1.10.py
@@ -193,6 +193,6 @@ def send_text(message):
 def handler_new_member(message):
     voice = open(helloNew[random.randint(0,coll_helloNew)], 'rb')
     bot.send_voice(message.chat.id, voice, reply_to_message_id=message.message_id)
-    bot.send_message(message.chat.id, 'https://telegra.ph/PRAVILA-CHATA-AniStar-Anime-07-04',,reply_to_message_id=message.message_id )
+    bot.send_message(message.chat.id, 'https://telegra.ph/PRAVILA-CHATA-AniStar-Anime-07-04',reply_to_message_id=message.message_id )
     
 bot.polling()
